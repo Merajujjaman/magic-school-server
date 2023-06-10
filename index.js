019910b3b16b7d90f26a33eb0b993a6c30a9bb51
Merajujjaman <asmmerajujjaman@gmail.com>
2023-06-11 03:50:53 +0600

    fix the mongodb problem

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
@@ -11,8 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 
-
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.zrkl84y.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.zrkl84y.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,15 +27,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usersCollection = client.db("magicDb").collection("users");
+
+    app.get('/users', async(req, res) => {
+        const result = await usersCollection.find().toArray()
+        console.log(result);
+        res.send(result)
+
+    })
+
+    app.post('/users', async (req, res) => {
+        const userData = req.body;
+        const query = { email: userData.email }
+        const existingUser = await usersCollection.findOne(query)
+        if (existingUser) {
+            return res.send({ message: 'already have an account' })
+        }
+        const result = await usersCollection.insertOne(userData);
+        res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+
+
 
 
 app.get('/', (req, res) => {
