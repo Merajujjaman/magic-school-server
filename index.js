3a7d442a4b55335f71aa3c1c9cd898d389502ca6
Merajujjaman <asmmerajujjaman@gmail.com>
2023-06-14 18:40:45 +0600

    vercel joson file added

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const usersCollection = client.db("magicDb").collection("users");
         const paymentsCollection = client.db("magicDb").collection("payments");
         const selectClassCollection = client.db("magicDb").collection("selectClass");
@@ -283,7 +283,7 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
